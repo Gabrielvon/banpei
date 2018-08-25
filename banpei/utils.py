4de1cca3e520d5b9bca0207c3175c15206cbdaca
Gabrielvon <fwlg1113@gmail.com>
2018-08-25 23:08:36 +0800

    update rolling_window function

--- banpei/utils.py
+++ banpei/utils.py
@@ -35,7 +35,7 @@ def power_method(A, iter_num=1):
     return u, s, v
 
 
-def _rolling_window(a, window):
+def rolling_window(arr, window, axis=0):
     """
     Usage:
     a = np.random.rand(30, 5)
@@ -47,12 +47,12 @@ def _rolling_window(a, window):
         roll along width(axis=1): rolling_window(a, 3).transpose(2, 0, 1, 3)
         roll along depth(axis=2): rolling_window(a.transpose(0, 2, 1), 3).transpose(3, 0, 2, 1)
     """
-    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
-    strides = a.strides + (a.strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
+    def _rolling_window(a, window):
+        shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+        strides = a.strides + (a.strides[-1],)
+        return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
-def rolling_window(arr, window, axis=0):
     if arr.ndim == 1:
         return _rolling_window(arr, window)
     elif arr.ndim == 2:
@@ -61,7 +61,8 @@ def rolling_window(arr, window, axis=0):
         elif axis == 1:
             return _rolling_window(arr, window).transpose(1, 0, 2)
         else:
-            raise Exception('AxisError: axis {} is out of bounds for array of dimension {}'.format(axis, arr.ndim))
+            raise Exception(
+                'AxisError: axis {} is out of bounds for array of dimension {}'.format(axis, arr.ndim))
     elif arr.ndim == 3:
         if axis == 0:
             return _rolling_window(arr.transpose(0, 2, 1), window).transpose(3, 0, 2, 1)
@@ -70,6 +71,7 @@ def rolling_window(arr, window, axis=0):
         elif axis == 2:
             return _rolling_window(arr.transpose(2, 1, 0), window).transpose(2, 3, 1, 0)
         else:
-            raise Exception('AxisError: axis {} is out of bounds for array of dimension {}'.format(axis, arr.ndim))
+            raise Exception(
+                'AxisError: axis {} is out of bounds for array of dimension {}'.format(axis, arr.ndim))
     else:
         return _rolling_window(arr, window)
